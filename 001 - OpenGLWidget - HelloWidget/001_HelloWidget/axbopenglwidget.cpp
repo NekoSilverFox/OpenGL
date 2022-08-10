ea51850a85c49d2db318f5369bd55a34bcf7d624
NekoSilverFox <weidufox@gmail.com>
2022-08-10 21:37:36 +0300

    增加 001 - HelloWidget

---
+++ 001 - OpenGLWidget - HelloWidget/001_HelloWidget/axbopenglwidget.cpp
@@ -0,0 +1,23 @@
+#include "axbopenglwidget.h"
+
+AXBOpenGLWidget::AXBOpenGLWidget(QWidget *parent) : QOpenGLWidget(parent)
+{
+
+}
+
+/* 首要要执行初始化过程，将函数指针指向显卡内的函数 */
+void AXBOpenGLWidget::initializeGL()
+{
+    initializeOpenGLFunctions();  // 【重点】初始化OpenGL函数，将 Qt 里的函数指针指向显卡的函数（头文件 QOpenGLFunctions_X_X_Core）
+}
+
+void AXBOpenGLWidget::resizeGL(int w, int h)
+{
+
+}
+
+void AXBOpenGLWidget::paintGL()
+{
+    glClearColor(0.2f, 0.3f, 0.32f, 1.0f);  // 【重点】如果没有 initializeGL，目前是一个空指针状态，没有指向显卡里面的函数
+    glClear(GL_COLOR_BUFFER_BIT);
+}
