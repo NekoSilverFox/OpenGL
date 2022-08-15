7d74e06f9b3131083f3bae7af794a0d0801ac133
NekoSilverFox <weidufox@gmail.com>
2022-08-15 22:27:23 +0300

    增加 VAO、VBO 代码绘制三角形

--- 001_OpenGLWidget_HelloWidget/001_HelloWidget/axbopenglwidget.cpp
+++ 001_OpenGLWidget_HelloWidget/001_HelloWidget/axbopenglwidget.cpp
@@ -18,6 +18,6 @@ void AXBOpenGLWidget::resizeGL(int w, int h)
 
 void AXBOpenGLWidget::paintGL()
 {
-    glClearColor(0.2f, 0.3f, 0.32f, 1.0f);  // 【重点】如果没有 initializeGL，目前是一个空指针状态，没有指向显卡里面的函数
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClearColor(0.2f, 0.3f, 0.32f, 1.0f);  // set方法【重点】如果没有 initializeGL，目前是一个空指针状态，没有指向显卡里面的函数，会报错
+    glClear(GL_COLOR_BUFFER_BIT);  // use方法
 }
