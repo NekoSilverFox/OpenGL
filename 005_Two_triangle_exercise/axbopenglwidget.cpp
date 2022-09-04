4d9491e1f447c0fda06232d172d4c695f3e964c2
NekoSilverFox <weidufox@gmail.com>
2022-09-04 23:04:07 +0300

    增加 005小练习代码

--- 005_Two_triangle_exercise/axbopenglwidget.cpp
+++ 005_Two_triangle_exercise/axbopenglwidget.cpp
@@ -2,14 +2,20 @@
 #include "QDebug"
 
 // 顶点数据
-float vertices[] = {
-  -0.5f, -0.5f, 0.0f,
-   0.5f, -0.5f, 0.0f,
-   0.0f,  0.5f, 0.0f
+float vertexFirstTriangle[] = {
+  -0.9f, -0.5f, 0.0f,
+   0.0f, -0.5f, 0.0f,
+  -0.45f,  0.5f, 0.0f
+};
+float vertexSecTriangle[] = {
+   0.0f, -0.5f, 0.0f,
+   0.9f, -0.5f, 0.0f,
+   0.45f,  0.5f, 0.0f
 };
 
 // 创建 VAO 和 VBO 对象并且赋予 ID
-unsigned int VBO, VAO;
+unsigned int VBOs[2];
+unsigned int VAOs[2];
 
 // 顶点着色器的源代码，顶点着色器就是把 xyz 原封不动的送出去
 const char *vertexShaderSource = "#version 330 core\n"
@@ -27,10 +33,16 @@ const char *fragmentShaderSource = "#version 330 core\n"
                                    "{\n"
                                    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
                                    "}\n\0";
+const char *fragmentShaderSource_left = "#version 330 core\n"
+                                   "out vec4 FragColor;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "   FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
+                                   "}\n\0";
 
 // 着色器程序（链接顶点和片段着色器之后生成的）
 unsigned int shaderProgram;
-
+unsigned int shaderProgram_left;
 
 AXBOpenGLWidget::AXBOpenGLWidget(QWidget *parent) : QOpenGLWidget(parent)
 {
@@ -45,47 +57,37 @@ void AXBOpenGLWidget::initializeGL()
 
     // ===================== VAO | VBO =====================
     // VAO 和 VBO 对象赋予 ID
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-
+    glGenVertexArrays(2,VAOs);
+    glGenBuffers(2, VBOs);
 
 
     // 绑定 VAO、VBO 对象
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindVertexArray(VAOs[0]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
 
+    /* 为当前绑定到 target 的缓冲区对象创建一个新的数据存储（在 GPU 上创建对应的存储区域，并将内存中的数据发送过去）*/
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexFirstTriangle), vertexFirstTriangle, GL_STATIC_DRAW);
 
-
-    /* 为当前绑定到 target 的缓冲区对象创建一个新的数据存储（在 GPU 上创建对应的存储区域，并将内存中的数据发送过去）
-        如果 data 不是 NULL，则使用来自此指针的数据初始化数据存储
-        void glBufferData(GLenum target,  // 需要在 GPU 上创建的目标
-                                            GLsizeipter size,  // 创建的显存大小
-                                            const GLvoid* data,  // 数据
-                                            GLenum usage)  // 创建在 GPU 上的哪一片区域（显存上的每个区域的性能是不一样的）https://registry.khronos.org/OpenGL-Refpages/es3.0/
-    */
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-
-    /* 告知显卡如何解析缓冲区里面的属性值
-        void glVertexAttribPointer(
-                                    GLuint index,  // VAO 中的第几个属性（VAO 属性的索引）
-                                    GLint size,  // VAO 中的第几个属性中对应的位置放几份数据
-                                    GLEnum type,  // 存放数据的数据类型
-                                    GLboolean normalized,  // 是否标准化
-                                    GLsizei stride,  // 步长
-                                    const void* offset  // 偏移量
-        )
-    */
+    /* 告知显卡如何解析缓冲区里面的属性值 */
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);  // 第一个属性，所以不需要偏移
 
-
-    // 开始 VAO 管理的第一个属性值
-    glEnableVertexAttribArray(0);
-
+    // 【重点】开始 VAO 管理的第一个属性值
+    glEnableVertexAttribArray(0);  // VAOs[0] 里面的第一个属性值
 
     // 解绑 VAO 和 VBO
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+
+
+    /* 第二个三角形 */
+    glBindVertexArray(VAOs[1]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexSecTriangle), vertexSecTriangle, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
 
 
     // ===================== 顶点着色器 =====================
@@ -102,28 +104,15 @@ void AXBOpenGLWidget::initializeGL()
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);  // 绑定至着色器原码
     glCompileShader(vertexShader);  // 编译着色器
 
-    /* 因为可能出错，所以进行错误检查，也就是判断时候成功编译 */
-    int success;  // 是否成功的标志
-    char infolog[512];  // 错误日志（信息）
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if(!success)
-    {
-      glGetShaderInfoLog(vertexShader, 512, NULL, infolog);
-      qDebug() << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infolog;
-    }
-
 
     // ===================== 片段着色器 =====================
     unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
 
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infolog);
-        qDebug() << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infolog;
-    }
+    unsigned int fragmentShader_left = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader_left, 1, &fragmentShaderSource_left, NULL);
+    glCompileShader(fragmentShader_left);
 
 
     // ===================== 链接着色器 =====================
@@ -133,17 +122,16 @@ void AXBOpenGLWidget::initializeGL()
     glAttachShader(shaderProgram, fragmentShader);  // 加入片段着色器
     glLinkProgram(shaderProgram);  // 链接
 
-    /* 因为可能出错，所以进行错误检查，也就是判断时候成功链接 */
-    glGetShaderiv(shaderProgram, GL_LINK_STATUS, &success);
-    if(!success)
-    {
-      glGetShaderInfoLog(shaderProgram, 512, NULL, infolog);
-      qDebug() << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infolog;
-    }
+    shaderProgram_left = glCreateProgram();
+    glAttachShader(shaderProgram_left, vertexShader);
+    glAttachShader(shaderProgram_left, fragmentShader_left);
+    glLinkProgram(shaderProgram_left);
+
 
     /* 删除已经不需要的编译的结果 */
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
+    glDeleteShader(fragmentShader_left);
 }
 
 void AXBOpenGLWidget::resizeGL(int w, int h)
@@ -157,12 +145,15 @@ void AXBOpenGLWidget::paintGL()
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);  // set方法【重点】如果没有 initializeGL，目前是一个空指针状态，没有指向显卡里面的函数，会报错
     glClear(GL_COLOR_BUFFER_BIT);  // use方法
 
+    /* 使用着色器 */
+    glUseProgram(shaderProgram);  // 【重点】当绘制多种颜色的时候要先使用着色器，不然上色会乱
     /* 重新绑定 VAO */
-    glBindVertexArray(VAO);
-
+    glBindVertexArray(VAOs[0]);
     /* 绘制三角形 */
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
-    /* 使用着色器 */
-    glUseProgram(shaderProgram);
+    /** 第二个三角形 **/
+    glUseProgram(shaderProgram_left);
+    glBindVertexArray(VAOs[1]);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 }
