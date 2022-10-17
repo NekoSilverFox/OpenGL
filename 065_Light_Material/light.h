01d0f7997aa9dfa803968a1dbc60d1867a59bdef
NekoSilverFox <weidufox@gmail.com>
2022-10-17 15:37:11 +0300

    增加 065_Light_Material 材质

---
+++ 065_Light_Material/light.h
@@ -0,0 +1,32 @@
+#ifndef LIGHT_H
+#define LIGHT_H
+#include <QVector3D>
+#include <QMatrix4x4>
+
+
+const float LENGTH_LIGHT = 1.0f;
+
+
+class Light
+{
+public:
+    Light();
+    Light(const float length, QVector3D color, QVector3D postion);
+
+
+private:
+    void _genVectorVerticesAndIndices();
+
+
+public:
+    QMatrix4x4 mat_model;
+    QVector3D postion;
+    QVector3D color;
+    std::vector<float> vertices;  // 顶点
+
+
+private:
+    float _length;
+};
+
+#endif // LIGHT_H
