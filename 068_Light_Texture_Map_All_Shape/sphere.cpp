b70dc676554aff60a1d5383de4de493b07c68fbf
NekoSilverFox <weidufox@gmail.com>
2022-10-21 18:45:15 +0300

    修复 球面异常（角度原因）

--- 068_Light_Texture_Map_All_Shape/sphere.cpp
+++ 068_Light_Texture_Map_All_Shape/sphere.cpp
@@ -26,7 +26,9 @@ unsigned int Sphere::getNumTrianglesinSphere()
 }
 
 
-
+/**
+ * 算法参考：https://dhbloo.github.io/2020/03/12/Sphere/#%E7%90%83%E5%9D%90%E6%A0%87%E8%BD%AC%E6%8D%A2
+ */
 void Sphere::_genVectorVerticesAndIndices()
 {
     /* 储存球上的所有的点 */
@@ -35,27 +37,23 @@ void Sphere::_genVectorVerticesAndIndices()
 
     /* 绘制上半球的所有顶点 */
     const unsigned int num_point_circle = 360.0f / _step_angle;  // 每个圈要绘制几个点
-    const unsigned int num_circle       = 360.0f / _step_angle;  // 半球上需要绘制圈圈的个数
-    qDebug() << "生成的顶点数量：" << num_point_circle;
+    const unsigned int num_circle       = 180.0f / _step_angle;  // 半球上需要绘制圈圈的个数
 
 
     /* 开始从最底部处一圈一圈的向上绘制，注意：最底部的点都是重叠了的 */
-    float current_xy_angle = -180.0f;
-    for (unsigned long i = 0; i < num_circle; i++)
+    float current_xy_angle = -90.0f;  // xy代表在[右]手坐标系下，x 轴与 y 轴的夹角
+    for (unsigned long i = 0; i <= num_circle; i++)
     {
         float current_xz_angle = 0.0f;  // 水平角度
-        qDebug() << "\n[INFO] ---> 生成第：" << i << "圈";
+        qDebug() << "\n[INFO] ---> 生成第：" << i << "圈， 垂直角度：" << current_xy_angle;
 
         /* 圈上的点 */
         for (unsigned long j = 0; j <= num_point_circle; j++)
         {
-
             /* 【重点 | 注意】sin() 默认是弧度！！，不是角度！！ */
             float x = _r * sin(current_xz_angle*M_PI/180) * cos(current_xy_angle*M_PI/180);
             float y = _r * sin(current_xy_angle*M_PI/180);
             float z = _r * cos(current_xz_angle*M_PI/180) * cos(current_xy_angle*M_PI/180);
-
-            qDebug() << "生成本圈上的第：" << j << "个点, 当前水平角度：" << current_xz_angle << "点：" << QVector3D(x, y, z);
 
             points.push_back(QVector3D(x, y, z));
 
@@ -65,17 +63,14 @@ void Sphere::_genVectorVerticesAndIndices()
     }
 
 
-
-    const int num_points = (num_point_circle + 1) * (num_circle - 1);
-    qDebug() << num_points;
-    for (int i = (num_point_circle + 1); i <= num_points; i++)
+    for (int i = 0; i < points.size(); i++)
     {
-        qDebug() << "当前 i = " << i << "取模：" << i % (num_point_circle + 1) << "真实顶点数量：" << points.size();
-//        if ((i % (num_point_circle + 1) == 0) && (i != 0))
-//        {
-//            qDebug() << "->跳过 i = " << i;
-//            continue;
-//        }
+//        qDebug() << "当前 i = " << i << "取模：" << i % (num_point_circle + 1) << "真实顶点数量：" << points.size();
+        if ((i % (num_point_circle + 1) == 0) && (i != 0))
+        {
+            qDebug() << "->跳过 i = " << i;
+            continue;
+        }
 
         /* 正三角 */
         QVector3D current_point          = points[i + 0];
@@ -96,7 +91,6 @@ void Sphere::_genVectorVerticesAndIndices()
         vertices.push_back(current_next_top_point);
         vertices.push_back(current_normal);
 
-#if 1
         /* 倒三角 */
                   current_point         = points[i + 0];
         QVector3D current_top_point     = points[i + 0 + num_point_circle + 1];
@@ -115,9 +109,6 @@ void Sphere::_genVectorVerticesAndIndices()
 
         vertices.push_back(current_top_next_point);
         vertices.push_back(current_normal);
-
-#endif
     }
-
-
+    qDebug() << "[INFO] 顶点生成结束";
 }
